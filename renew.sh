a99bf6deb9cd5613009a9a7ae62ecef401610496
tomahawksmail <tomahawksmail@gmail.com>
2024-02-27 14:57:49 -0800

    initial project with app

--- renew.sh
+++ renew.sh
@@ -7,4 +7,4 @@ echo crate service..............................................................
 docker service create --name php_8.3-helpdesk\
                       --replicas 5 \
                       --publish published=5541,target=5541 \
-                        php_8.3-helpdesk
+                        php_8.3-helpdesk:1.0
