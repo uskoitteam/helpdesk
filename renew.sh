5844a340cbcde9dbfecdde6e53cd4c247a31ba4c
tomahawksmail <tomahawksmail@gmail.com>
2024-02-27 14:58:57 -0800

    initial project with app

--- renew.sh
+++ renew.sh
@@ -4,7 +4,4 @@ git pull origin
 
 docker build -t php_8.3-helpdesk .
 echo crate service.....................................................................................
-docker service create --name php_8.3-helpdesk\
-                      --replicas 5 \
-                      --publish published=5541,target=5541 \
-                        php_8.3-helpdesk:1.0
+docker service create --name php_8.3-helpdesk --replicas 3 --publish 8080:80 php_8.3-helpdesk
