60a0d811dfbbd896285b9a0ac1a4a44d27c30c22
tomahawksmail <tomahawksmail@gmail.com>
2024-02-28 10:09:21 -0800

    initial project with app

--- renew.sh
+++ renew.sh
@@ -1,19 +1,6 @@
 echo crate image.......................................................................................
-docker image build -t helpdesk:1 .
+docker build -t php-helpdesk .
 
 echo crate service.....................................................................................
-docker service create --name helpdesk\
-                      --replicas 1 \
-                      --publish published=5549,target=5549 \
-                        helpdesk:1
+docker stack deploy --compose-file docker-compose2.yml php-helpdesk-dervice
 
-
-
-#                        docker run -d -p 5549:5549 helpdesk
-
-
-
-
-
-# docker build -t php-helpdesk .
-#
